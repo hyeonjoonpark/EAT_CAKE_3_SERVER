37f65636c5fbf989bcc9ecee981965b791c69a52
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-18 16:05:20 +0900

    FEAT: user

--- src/main/java/eat/cake/team3/domain/user/User.java
+++ src/main/java/eat/cake/team3/domain/user/User.java
@@ -1,4 +1,45 @@
 package eat.cake.team3.domain.user;
 
+import eat.cake.team3.domain.user.types.GenderType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+  @Id
+  private Long id;
+  private String provider;
+  private String providerId;
+  private String email;
+  private String userName;
+  private Date birth;
+  private String phoneNumber;
+  @Enumerated(EnumType.STRING)
+  private GenderType gender;
+  private int age;
+  @Column(columnDefinition = "LONGBLOB")
+  private String profileImage;
+  private String refreshToken;
+
+  @Builder
+  public User(Long id, String provider, String providerId, String email, String userName, Date birth, String phoneNumber, GenderType gender, int age, String profileImage, String refreshToken) {
+    this.id = id;
+    this.provider = provider;
+    this.providerId = providerId;
+    this.email = email;
+    this.userName = userName;
+    this.birth = birth;
+    this.phoneNumber = phoneNumber;
+    this.gender = gender;
+    this.age = age;
+    this.profileImage = profileImage;
+    this.refreshToken = refreshToken;
+  }
 }
---
+++ src/main/java/eat/cake/team3/domain/user/types/GenderType.java
@@ -0,0 +1,6 @@
+package eat.cake.team3.domain.user.types;
+
+public enum GenderType {
+  MALE,
+  FEMALE
+}
