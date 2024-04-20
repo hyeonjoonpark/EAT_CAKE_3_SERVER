de07ed02d70085335840105fa5bf44468f186412
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-20 10:25:00 +0900

    ADD: Goal Service

---
+++ src/main/java/eat/cake/team3/domain/goal/service/implement/GoalServiceImpl.java
@@ -0,0 +1,9 @@
+package eat.cake.team3.domain.goal.service.implement;
+
+import eat.cake.team3.domain.goal.service.interfaces.GoalService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoalServiceImpl implements GoalService {
+
+}
---
+++ src/main/java/eat/cake/team3/domain/goal/service/interfaces/GoalService.java
@@ -0,0 +1,8 @@
+package eat.cake.team3.domain.goal.service.interfaces;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface GoalService {
+
+}
